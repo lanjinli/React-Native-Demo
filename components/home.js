0130e9c5cd0468bff542ee1bf9c2502774c8305a
lanjinli <li.jin@qq.com>
2018-11-19 22:24:20 +0800

    修改首页弹窗，新增首页抽屉

--- components/home.js
+++ components/home.js
@@ -21,6 +21,10 @@ const TopHeight = Platform.select({
     ios: 20,
     android: 0,
 });
+const OffsetHeader = Platform.select({
+    ios: 0,
+    android: 15,
+});
 
 //定义首页
 class HomePage extends Component {
@@ -30,6 +34,32 @@ class HomePage extends Component {
     };
 
     state = {
+        homeList: [
+            {
+                img: require('../assets/images/home_icon_01.png'),
+                title: '计算机视觉',
+                summary: '',
+                page: ''
+            },
+            {
+                img: require('../assets/images/home_icon_02.png'),
+                title: '自然语言处理',
+                summary: '',
+                page: ''
+            },
+            {
+                img: require('../assets/images/home_icon_03.png'),
+                title: '智能语音交互',
+                summary: '',
+                page: ''
+            },
+            {
+                img: require('../assets/images/home_icon_04.png'),
+                title: '底层硬件通讯',
+                summary: '',
+                page: ''
+            }
+        ],
         modalInfoVisible: false
     };
 
@@ -42,7 +72,7 @@ class HomePage extends Component {
             <View style={[styles.flex, { backgroundColor: '#FFF' }]}>
                 <View style={styles.header}>
                     <View style={styles.h_nav}>
-                        <TouchableOpacity style={styles.h_n_btn} activeOpacity={0.7}>
+                        <TouchableOpacity style={styles.h_n_btn} activeOpacity={0.7} onPress={() => (this.props.navigation.openDrawer())} >
                             <Image style={{ width: 48, height: 48 }} source={require("../assets/images/icon_menu.png")} />
                         </TouchableOpacity>
                         <View style={styles.h_title}>
@@ -55,42 +85,20 @@ class HomePage extends Component {
                 </View>
                 <ScrollView style={styles.body}>
                     <View style={styles.b_list}>
-                        <TouchableOpacity
-                            style={styles.b_l_btn}
-                            activeOpacity={0.9}
-                        >
-                            <LinearGradient start={{ x: 0.25, y: 0.25 }} end={{ x: 0.75, y: 0.75 }} colors={['#0079f4', '#1b4fd5']} style={styles.b_l_t_bg}>
-                                <Image source={require("../assets/images/home_icon_01.png")} style={styles.b_l_btn_icon} />
-                                <Text style={styles.b_l_btn_text}>计算机视觉</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.b_l_btn}
-                            activeOpacity={0.9}
-                        >
-                            <LinearGradient start={{ x: 0.25, y: 0.25 }} end={{ x: 0.75, y: 0.75 }} colors={['#0079f4', '#1b4fd5']} style={styles.b_l_t_bg}>
-                                <Image source={require("../assets/images/home_icon_02.png")} style={styles.b_l_btn_icon} />
-                                <Text style={styles.b_l_btn_text}>自然语言处理</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.b_l_btn}
-                            activeOpacity={0.9}
-                        >
-                            <LinearGradient start={{ x: 0.25, y: 0.25 }} end={{ x: 0.75, y: 0.75 }} colors={['#0079f4', '#1b4fd5']} style={styles.b_l_t_bg}>
-                                <Image source={require("../assets/images/home_icon_03.png")} style={styles.b_l_btn_icon} />
-                                <Text style={styles.b_l_btn_text}>智能语音交互</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.b_l_btn}
-                            activeOpacity={0.9}
-                        >
-                            <LinearGradient start={{ x: 0.25, y: 0.25 }} end={{ x: 0.75, y: 0.75 }} colors={['#0079f4', '#1b4fd5']} style={styles.b_l_t_bg}>
-                                <Image source={require("../assets/images/home_icon_04.png")} style={styles.b_l_btn_icon} />
-                                <Text style={styles.b_l_btn_text}>底层硬件通讯</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
+                        {
+                            this.state.homeList.map((item, index) => {
+                                return (<TouchableOpacity
+                                    style={styles.b_l_btn}
+                                    activeOpacity={0.9}
+                                    key={index}
+                                >
+                                    <LinearGradient start={{ x: 0.25, y: 0.25 }} end={{ x: 0.75, y: 0.75 }} colors={['#0079f4', '#1b4fd5']} style={styles.b_l_t_bg}>
+                                        <Image source={item.img} style={styles.b_l_btn_icon} />
+                                        <Text style={styles.b_l_btn_text}>{item.title}</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>);
+                            })
+                        }
                     </View>
                     <View style={styles.b_foot}>
                         <Text style={styles.b_f_text}>腾讯AI开放平台</Text>
@@ -98,38 +106,86 @@ class HomePage extends Component {
                 </ScrollView>
                 <Modal
                     style={styles.modal_info}
-                    animationType='fade'
-                    transparent={true}
+                    animationType='slide'
+                    transparent={false}
                     visible={this.state.modalInfoVisible}
                     onRequestClose={() => { this.setModalInfoVisible(!this.state.modalInfoVisible) }}
                 >
-                    <View style={[styles.flex, { justifyContent: 'space-around', backgroundColor: 'rgba(0, 0, 0, 0.5)' }]}>
-                        <View style={styles.modal_body}>
-                            <Text style={styles.h_t_text}>描述信息</Text>
-                        </View>
-                        <TouchableOpacity style={styles.h_n_btn} activeOpacity={0.7} onPress={() => {this.setModalInfoVisible(!this.state.modalInfoVisible)}}>
-                            <Image style={{ width: 48, height: 48 }} source={require("../assets/images/icon_down.png")} />
+                    <View style={styles.modal_hred}>
+                        <TouchableOpacity style={styles.modal_info_back} activeOpacity={0.7} onPress={() => { this.setModalInfoVisible(!this.state.modalInfoVisible) }}>
+                            <Image tintColor={'#808080'} style={{ width: 48, height: 48 }} source={require("../assets/images/icon_back.png")} />
                         </TouchableOpacity>
                     </View>
+                    <ScrollView style={styles.modal_body}>
+                        <Text style={styles.modal_title}>腾讯三大AI实验室  强大的技术基石</Text>
+                        <Text style={styles.modal_summary}>AI LAB</Text>
+                        <Text style={styles.modal_text}>腾讯AI Lab聚集全球数十位人工智能科学家、70位世界一流AI博士，专注机器学习、计算机视觉、语音识别、自然语言处理等人工智能领域的研究。</Text>
+                        <Text style={styles.modal_summary}>优图实验室</Text>
+                        <Text style={styles.modal_text}>优图实验室致力于人脸识别、图像识别及声音识别的技术研究和产品研发，拥有业界独创的活体检测技术，已为上百家企业提供服务。</Text>
+                        <Text style={styles.modal_summary}>微信AI</Text>
+                        <Text style={styles.modal_text}>微信AI致力于为语音识别、自然语言处理、计算机视觉、数据挖掘和机器学习等人工智能技术的发展带来革命性进步。</Text>
+                    </ScrollView>
                 </Modal>
             </View>
         );
     }
 }
 
-export default StackNavigator(
+// export default StackNavigator(
+//     {
+//         Home: {
+//             screen: HomePage,
+//         }
+//     },
+//     {
+//         initialRouteName: 'Home',
+//         navigationOptions: {
+//             header: () => null
+//         },
+//         mode: 'card',
+//         headerMode: 'screen'
+//     }
+// );
+
+// 定义下一页
+// class SeconPage extends Component {
+//     static navigationOptions = {
+//         title: '通知',
+//         drawerLabel: '通知',
+//     };
+//     //点击返回上一页
+//     pressPop() {
+//         this.props.navigation.navigate('Home');
+//     };
+//     render() {
+//         return (
+//             <View style={[styles.flex, { backgroundColor: 'cyan' }]}>
+//                 <TouchableOpacity style={[styles.btn]} onPress={this.pressPop.bind(this)}>
+//                     <Text>点击返回一页面</Text>
+//                 </TouchableOpacity>
+//             </View>
+//         );
+//     }
+// }
+
+export default DrawerNavigator(
     {
         Home: {
             screen: HomePage,
         }
     },
     {
-        initialRouteName: 'Home',
-        navigationOptions: {
-            header: () => null
-        },
-        mode: 'card',
-        headerMode: 'screen'
+        drawerWidth: 220, // 抽屉宽
+        drawerPosition: 'left', // 抽屉在左边还是右边
+        // contentComponent: CustomDrawerContentComponent,  // 自定义抽屉组件
+        contentOptions: {
+            initialRouteName: HomePage, // 默认页面组件
+            activeTintColor: '#008AC9',  // 选中文字颜色
+            activeBackgroundColor: '#f5f5f5', // 选中背景颜色
+            inactiveTintColor: '#000',  // 未选中文字颜色
+            inactiveBackgroundColor: '#fff', // 未选中背景颜色
+            style: {}
+        }
     }
 );
 
@@ -140,6 +196,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#fff',
+        position: 'relative'
     },
     header: {
         width: width,
@@ -167,15 +224,14 @@ const styles = StyleSheet.create({
         fontSize: 18,
     },
     body: {
-        height: height - (48 + TopHeight),
+        height: height - (48 + TopHeight) + OffsetHeader,
         position: 'absolute',
         top: 48 + TopHeight,
-        // height: height - TopHeight,
         backgroundColor: '#fff',
         width: width
     },
     b_list: {
-        backgroundColor: '#fff',
+        // backgroundColor: '#fff',
         flex: 1,
         minHeight: height - (92 + TopHeight),
     },
@@ -216,11 +272,41 @@ const styles = StyleSheet.create({
         fontSize: 12,
         color: '#bdbdbd',
     },
-    modal_body: {
-        width: width - 60,
-        height: height - (height/4),
+    modal_hred: {
+        width: width,
+        position: 'absolute',
+        top: TopHeight,
+        zIndex: 1,
         backgroundColor: '#fff',
-        borderRadius: 3,
-        marginTop: 30
+        height: 48
+    },
+    modal_info_back: {
+        width: 48,
+        height: 48
+    },
+    modal_body: {
+        position: 'absolute',
+        top: 48 + TopHeight,
+        width: width,
+        height: height - (48 + TopHeight)
+    },
+    modal_title: {
+        fontSize: 18,
+        textAlign: 'center',
+        paddingVertical: 30,
+    },
+    modal_summary: {
+        fontSize: 14,
+        lineHeight: 22,
+        paddingTop: 20,
+        paddingHorizontal: 10,
+    },
+    modal_text: {
+        fontSize: 14,
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        textAlign: 'justify',
+        lineHeight: 22,
+        color: '#545454'
     }
 });
